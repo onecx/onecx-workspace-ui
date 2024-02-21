1344724f51299a712f9e01188d6cbf96679c94a2
Christian Badura <christian.badura@capgemini.com>
2024-02-21 10:59:37 +0100

    fix: paste all prev used spec files

--- src/app/workspace/workspace-import/confirm/confirm.component.spec.ts
+++ src/app/workspace/workspace-import/confirm/confirm.component.spec.ts
@@ -1,131 +1,131 @@
-// import { NO_ERRORS_SCHEMA } from '@angular/core'
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
-// // import { HttpClient } from '@angular/common/http'
-// import { HttpClientTestingModule } from '@angular/common/http/testing'
-// // import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
-// import { of } from 'rxjs'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+// import { HttpClient } from '@angular/common/http'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { of } from 'rxjs'
 
-// // import { HttpLoaderFactory } from 'src/app/shared/shared.module'
-// import { ConfirmComponent } from './confirm.component'
-// import { WorkspaceAPIService } from '../../../shared/generated'
+// import { HttpLoaderFactory } from 'src/app/shared/shared.module'
+import { ConfirmComponent } from './confirm.component'
+import { WorkspaceAPIService } from '../../../shared/generated'
 
-// const portal = {
-//   portalName: 'name',
-//   portalRoles: ['role'],
-//   themeName: 'theme',
-//   baseUrl: 'url',
-//   tenantId: 'id'
-// }
+const workspace = {
+  workspaceName: 'name',
+  workspaceRoles: ['role'],
+  themeName: 'theme',
+  baseUrl: 'url',
+  tenantId: 'id'
+}
 
-// describe('ConfirmComponent', () => {
-//   let component: ConfirmComponent
-//   let fixture: ComponentFixture<ConfirmComponent>
+describe('ConfirmComponent', () => {
+  let component: ConfirmComponent
+  let fixture: ComponentFixture<ConfirmComponent>
 
-//   const apiServiceSpy = {
-//     getAllPortals: jasmine.createSpy('getAllPortals').and.returnValue(of({}))
-//   }
-//   const themeServiceSpy = jasmine.createSpyObj('ThemeService', ['getThemes'])
-//   themeServiceSpy.getThemes.and.returnValue(
-//     of([
-//       { label: undefined, value: 'theme1' },
-//       { label: undefined, value: 'theme2' }
-//     ])
-//   )
+  const apiServiceSpy = {
+    searchWorkspaces: jasmine.createSpy('searchWorkspaces').and.returnValue(of({}))
+  }
+  const themeServiceSpy = jasmine.createSpyObj('ThemeService', ['getThemes'])
+  themeServiceSpy.getThemes.and.returnValue(
+    of([
+      { label: undefined, value: 'theme1' },
+      { label: undefined, value: 'theme2' }
+    ])
+  )
 
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ConfirmComponent],
-//       imports: [
-//         HttpClientTestingModule
-//         // TranslateModule.forRoot({
-//         //   loader: {
-//         //     provide: TranslateLoader,
-//         //     useFactory: HttpLoaderFactory,
-//         //     deps: [HttpClient]
-//         //   }
-//         // })
-//       ],
-//       schemas: [NO_ERRORS_SCHEMA],
-//       providers: [
-//         { provide: WorkspaceAPIService, useValue: apiServiceSpy }
-//         // { provide: ThemesAPIService, useValue: themeServiceSpy }
-//       ]
-//     }).compileComponents()
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ConfirmComponent],
+      imports: [
+        HttpClientTestingModule
+        // TranslateModule.forRoot({
+        //   loader: {
+        //     provide: TranslateLoader,
+        //     useFactory: HttpLoaderFactory,
+        //     deps: [HttpClient]
+        //   }
+        // })
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: WorkspaceAPIService, useValue: apiServiceSpy }
+        // { provide: ThemesAPIService, useValue: themeServiceSpy }
+      ]
+    }).compileComponents()
 
-//     apiServiceSpy.getAllPortals.calls.reset()
-//   }))
+    apiServiceSpy.searchWorkspaces.calls.reset()
+  }))
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ConfirmComponent)
-//     component = fixture.componentInstance
-//     fixture.detectChanges()
-//   })
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy()
-//   })
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
 
-//   it('should reflect missing baseUrl and fetch portals OnInit', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([]))
-//     component.baseUrl = ''
-//     spyOn(component, 'checkPortalUniqueness')
+  it('should reflect missing baseUrl and fetch portals OnInit', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([]))
+    component.baseUrl = ''
+    spyOn(component, 'checkWorkspaceUniqueness')
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.checkPortalUniqueness).toHaveBeenCalled()
-//     expect(component.baseUrlIsMissing).toBeTrue()
-//   })
+    expect(component.checkWorkspaceUniqueness).toHaveBeenCalled()
+    expect(component.baseUrlIsMissing).toBeTrue()
+  })
 
-//   it('should also fetch themes OnInit if themeName exists', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([]))
-//     themeServiceSpy.getThemes.and.returnValue(of([]))
-//     spyOn(component, 'checkThemeNames')
-//     component.themeName = 'name'
+  it('should also fetch themes OnInit if themeName exists', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([]))
+    themeServiceSpy.getThemes.and.returnValue(of([]))
+    spyOn(component, 'checkThemeNames')
+    component.themeName = 'name'
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.checkThemeNames).toHaveBeenCalled()
-//   })
+    expect(component.checkThemeNames).toHaveBeenCalled()
+  })
 
-//   it('should set portalTenantExists to true in checkPortalUniqueness onInit if permission', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([portal]))
-//     component.hasPermission = true
-//     component.portalName = 'name'
-//     component.tenantId = 'id'
+  it('should set workspaceTenantExists to true in checkWorkspaceUniqueness onInit if permission', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([workspace]))
+    component.hasPermission = true
+    component.workspaceName = 'name'
+    component.tenantId = 'id'
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.portalTenantExists).toBeTrue()
-//   })
+    expect(component.workspaceTenantExists).toBeTrue()
+  })
 
-//   it('should set portalNameExists to true in checkPortalUniqueness onInit if no permission', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([portal]))
-//     component.hasPermission = false
-//     component.portalName = 'name'
+  it('should set workspaceNameExists to true in checkWorkspaceUniqueness onInit if no permission', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([workspace]))
+    component.hasPermission = false
+    component.workspaceName = 'name'
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.portalNameExists).toBeTrue()
-//   })
+    expect(component.workspaceNameExists).toBeTrue()
+  })
 
-//   it('should set baseUrlExists to true in checkPortalUniqueness onInit', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([portal]))
-//     component.baseUrl = 'url'
-//     component.baseUrlIsMissing = false
+  it('should set baseUrlExists to true in checkWorkspaceUniqueness onInit', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([workspace]))
+    component.baseUrl = 'url'
+    component.baseUrlIsMissing = false
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.baseUrlExists).toBeTrue()
-//   })
+    expect(component.baseUrlExists).toBeTrue()
+  })
 
-//   it('should set themeNameExists to true in checkPortalUniqueness onInit', () => {
-//     apiServiceSpy.getAllPortals.and.returnValue(of([portal]))
-//     themeServiceSpy.getThemes.and.returnValue(of([{ name: 'theme' }]))
-//     component.hasPermission = false
-//     component.themeName = 'theme'
+  it('should set themeNameExists to true in checkWorkspaceUniqueness onInit', () => {
+    apiServiceSpy.searchWorkspaces.and.returnValue(of([workspace]))
+    themeServiceSpy.getThemes.and.returnValue(of([{ name: 'theme' }]))
+    component.hasPermission = false
+    component.themeName = 'theme'
 
-//     component.ngOnInit()
+    component.ngOnInit()
 
-//     expect(component.themeNameExists).toBeTrue()
-//   })
-// })
+    expect(component.themeNameExists).toBeTrue()
+  })
+})
