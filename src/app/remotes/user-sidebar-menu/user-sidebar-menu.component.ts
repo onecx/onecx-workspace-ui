4fe05725a540652318e23e816b7ed23deae61923
Bastian Jakobi <bastian.jakobi@capgemini.com>
2024-04-04 12:13:15 +0200

    refactor: add initial component files

---
+++ src/app/remotes/user-sidebar-menu/user-sidebar-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-user-sidebar-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-sidebar-menu.component.html',
+  styleUrls: ['./user-sidebar-menu.component.scss']
+})
+export class UserSidebarMenuComponent {}
