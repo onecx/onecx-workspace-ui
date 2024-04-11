bf4ef1fe295a41ea2deeb7854edbc851d8a9c290
kim.tran <kim.tran@capgemini.com>
2024-04-11 10:43:22 +0200

    fix: use primeng panelmenu

--- src/app/remotes/main-menu/main-menu.component.ts
+++ src/app/remotes/main-menu/main-menu.component.ts
@@ -1,28 +1,28 @@
 import { CommonModule, Location } from '@angular/common'
 import { HttpClient } from '@angular/common/http'
-import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core'
-import { Router, RouterModule } from '@angular/router'
+import { Component, Inject, OnInit } from '@angular/core'
+import { RouterModule } from '@angular/router'
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
 import {
   AngularRemoteComponentsModule,
   BASE_URL,
+  RemoteComponentConfig,
   ocxRemoteComponent,
-  provideTranslateServiceForRoot,
-  RemoteComponentConfig
+  provideTranslateServiceForRoot
 } from '@onecx/angular-remote-components'
 import {
   AppStateService,
-  createRemoteComponentTranslateLoader,
   PortalCoreModule,
-  UserService
+  UserService,
+  createRemoteComponentTranslateLoader
 } from '@onecx/portal-integration-angular'
 import { MenuItem } from 'primeng/api'
-import { map, mergeMap, Observable, ReplaySubject, shareReplay, withLatestFrom } from 'rxjs'
+import { PanelMenuModule } from 'primeng/panelmenu'
+import { Observable, ReplaySubject, map, mergeMap, shareReplay, withLatestFrom } from 'rxjs'
 import { Configuration, UserMenuAPIService, UserWorkspaceMenuItem } from 'src/app/shared/generated'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { environment } from 'src/environments/environment'
-import { SubMenuComponent } from '../sub-menu/sub-menu.component'
 
 @Component({
   selector: 'app-main-menu',
@@ -35,7 +35,7 @@ import { SubMenuComponent } from '../sub-menu/sub-menu.component'
     RouterModule,
     TranslateModule,
     SharedModule,
-    SubMenuComponent
+    PanelMenuModule
   ],
   providers: [
     {
@@ -55,49 +55,43 @@ import { SubMenuComponent } from '../sub-menu/sub-menu.component'
 @UntilDestroy()
 export class OneCXMainMenuComponent implements ocxRemoteComponent, OnInit {
   menuItems$: Observable<MenuItem[]> | undefined
-  remoteComponentConfig: RemoteComponentConfig | undefined
 
   constructor(
     @Inject(BASE_URL) private baseUrl: ReplaySubject<string>,
     private userService: UserService,
     private translateService: TranslateService,
     private appStateService: AppStateService,
-    private userMenuService: UserMenuAPIService,
-    private router: Router,
-    private changDetectorRef: ChangeDetectorRef
+    private userMenuService: UserMenuAPIService
   ) {
     this.userService.lang$.subscribe((lang) => this.translateService.use(lang))
-  }
-  ngOnInit(): void {
-    this.menuItems$ = this.getMenuItems()
   }
 
   ocxInitRemoteComponent(remoteComponentConfig: RemoteComponentConfig) {
     this.baseUrl.next(remoteComponentConfig.baseUrl)
-    this.remoteComponentConfig = remoteComponentConfig
     this.userMenuService.configuration = new Configuration({
       basePath: Location.joinWithSlash(remoteComponentConfig.baseUrl, environment.apiPrefix)
     })
   }
 
-  getMenuItems(forceReload = false): Observable<MenuItem[]> {
-    if (!this.menuItems$ || forceReload) {
-      this.menuItems$ = this.appStateService.currentWorkspace$.pipe(
-        mergeMap((currentWorkspace) =>
-          this.userMenuService.getUserMenu({
-            userWorkspaceMenuRequest: {
-              workspaceName: currentWorkspace.portalName,
-              menuKeys: ['main-menu']
-            }
-          })
-        ),
-        withLatestFrom(this.userService.lang$),
-        map(([data, userLang]) => this.constructMenuItems(data.menu?.[0].children, userLang)),
-        shareReplay(),
-        untilDestroyed(this)
-      )
-    }
-    return this.menuItems$
+  ngOnInit(): void {
+    this.getMenuItems()
+  }
+
+  getMenuItems() {
+    this.menuItems$ = this.appStateService.currentWorkspace$.pipe(
+      mergeMap((currentWorkspace) =>
+        this.userMenuService.getUserMenu({
+          userWorkspaceMenuRequest: {
+            workspaceName: currentWorkspace.portalName,
+            menuKeys: ['main-menu']
+          }
+        })
+      ),
+      withLatestFrom(this.userService.lang$),
+      map(([data, userLang]) => this.constructMenuItems(data.menu?.[0].children, userLang)),
+      shareReplay(),
+      untilDestroyed(this)
+    )
   }
 
   private constructMenuItems(userWorkspaceMenuItem: UserWorkspaceMenuItem[] | undefined, userLang: string): MenuItem[] {
@@ -119,7 +113,7 @@ export class OneCXMainMenuComponent implements ocxRemoteComponent, OnInit {
     let label: string | undefined
 
     if (item) {
-      isLocal = this.isLocal(item.url)
+      isLocal = !item.external
       label = item.i18n ? item.i18n[userLang] || item.name : ''
 
       return {
@@ -132,51 +126,18 @@ export class OneCXMainMenuComponent implements ocxRemoteComponent, OnInit {
                 .map((i) => this.mapMenuItem(i, userLang))
             : undefined,
         label,
-        icon: item.badge || undefined,
+        icon: item.badge ? 'pi pi-' + item.badge : undefined,
         routerLink: isLocal ? this.stripBaseHref(item.url) : undefined,
-        routerLinkActiveOptions: [{ exact: false }],
-        url: isLocal ? undefined : item.url,
-        badge: isLocal ? 'mf' : 'ext'
+        url: isLocal ? undefined : item.url
       }
     } else {
       return {}
     }
   }
 
-  private toRouteUrl(url: string | undefined) {
-    if (!url) {
-      return url
-    }
-    if (url?.startsWith('/')) {
-      url = url.substring(1)
-    }
-    if (url.endsWith('/')) {
-      url = url.substring(0, url.length - 1)
-    }
-    return url
-  }
-
-  private isLocal(url: string | undefined) {
-    if (url && url.startsWith('http')) {
-      return false
-    }
-    const path = url?.trim().split('?')[0]
-    if (
-      this.router.config.some((r) => {
-        return this.toRouteUrl(path) === this.toRouteUrl(this.remoteComponentConfig?.baseUrl.concat(r.path ?? ''))
-      })
-    ) {
-      return true
-    }
-
-    return false
-  }
-
   private stripBaseHref(url: string | undefined): string | undefined {
-    if (this.remoteComponentConfig?.baseUrl && url) {
-      return url.replace(this.remoteComponentConfig?.baseUrl, '')
-    } else {
-      return url
-    }
+    const basePath = document.getElementsByTagName('base')[0]?.href
+    const baseUrl = new URL(basePath, window.location.origin).toString()
+    return url?.replace(baseUrl, '')
   }
 }
