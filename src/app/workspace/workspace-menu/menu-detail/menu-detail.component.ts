22bbed92eb39189eb08d886386bdef4f00352899
Henry Taeschner <henry.taeschner@capgemini.com>
2024-05-14 20:15:44 +0200

    fix: code smells in menu detail

--- src/app/workspace/workspace-menu/menu-detail/menu-detail.component.ts
+++ src/app/workspace/workspace-menu/menu-detail/menu-detail.component.ts
@@ -172,7 +172,7 @@ export class MenuDetailComponent implements OnChanges {
         position: this.menuItem.position,
         disabled: this.menuItem.disabled,
         external: this.menuItem.external,
-        url: this.prepareUrlObject(this.menuItem.url),
+        url: this.prepareUrlList(this.menuItem.url),
         description: this.menuItem.description
       })
     }
@@ -185,44 +185,48 @@ export class MenuDetailComponent implements OnChanges {
    * 2. If url is http address or unknown => add a specific item for it
    * 3. Add an empty item on top (to clean the field by selection = no url)
    */
-  private prepareUrlObject(url?: string): MFE | null {
-    //if (!url) return null
+  private prepareUrlList(url?: string): MFE | null {
     let mfe: MFE | null = null
-    let maxLength = 0
     let itemCreated = false
     if (url?.match(/^(http|https)/g)) {
       mfe = { appId: '$$$-http-address', basePath: url, product: 'MENU_ITEM.URL.HTTP' } as MFE
       itemCreated = true
-    }
-    if (url) {
+    } else if (url) {
       // search for mfe with best match of base path
-      for (const mfeItem of this.mfeItems) {
-        const bp = mfeItem.basePath!
-        // perfect match
-        if (url === bp) {
-          mfe = mfeItem
-          break
-        }
-        // if URL was extended then create such specific item with best match
-        if (url?.toLowerCase().startsWith(bp.toLowerCase()) && maxLength < bp.length) {
-          mfe = { ...mfeItem }
-          maxLength = bp.length // remember length for finding the best match
-          mfe.basePath = url
-          itemCreated = true
-        }
-      }
-      if (!mfe) {
-        mfe = { appId: '$$$-unknown-product', basePath: url, product: 'MENU_ITEM.URL.UNKNOWN.PRODUCT' } as MFE
-        itemCreated = true
-      }
+      const match = this.searchMfeForBasePathMatch(url)
+      mfe = match[0]
+      itemCreated = match[1]
     }
-    if (mfe && itemCreated) {
-      this.mfeItems.unshift(mfe) // add on top
-    }
+    if (mfe && itemCreated) this.mfeItems.unshift(mfe) // add the new one on top
     this.mfeItems.unshift({ appId: '$$$-empty', basePath: '', product: 'MENU_ITEM.URL.EMPTY' })
     return url ? mfe : this.mfeItems[0]
   }
 
+  private searchMfeForBasePathMatch(url: string): [MFE, boolean] {
+    let mfe: MFE | null = null
+    let maxLength = 0
+    let itemCreated = false
+    for (const mfeItem of this.mfeItems) {
+      const bp = mfeItem.basePath!
+      // perfect match
+      if (url === bp) {
+        mfe = mfeItem
+        break
+      }
+      // if URL was extended then create such specific item with best match
+      if (url?.toLowerCase().startsWith(bp.toLowerCase()) && maxLength < bp.length) {
+        mfe = { ...mfeItem }
+        mfe.basePath = url
+        maxLength = bp.length // remember length for finding the best match
+        itemCreated = true
+      }
+    }
+    if (!mfe) {
+      mfe = { appId: '$$$-unknown-product', basePath: url, product: 'MENU_ITEM.URL.UNKNOWN.PRODUCT' } as MFE
+      itemCreated = true
+    }
+    return [mfe, itemCreated]
+  }
   /***************************************************************************
    * SAVE => CREATE + UPDATE
    **************************************************************************/
@@ -388,9 +392,9 @@ export class MenuDetailComponent implements OnChanges {
               }
             }
           }
-          console.log('loadMfeUrls() - mfeItems:', this.mfeItems)
-          this.filteredMfes = this.mfeItems.sort(this.sortMfesByProductAndBasePath)
-          this.fillForm() // now the form can be filled
+          //console.log('loadMfeUrls() - mfeItems:', this.mfeItems)
+          this.mfeItems.sort(this.sortMfesByProductAndBasePath)
+          this.fillForm()
         }),
         catchError((err) => {
           console.error('getProductsForWorkspaceId():', err)
@@ -413,14 +417,26 @@ export class MenuDetailComponent implements OnChanges {
   public onFocusUrl(field: any): void {
     field.overlayVisible = true
   }
-  public onClearPath(): void {
+  // ignore url value and show all paths
+  public onDropdownClick(field: any): void {
+    field.overlayVisible = true
+    this.filteredMfes = this.mfeItems
+  }
+  // inkremental filtering: search path with current value after key up
+  public onKeyUpUrl(ev: Event): void {
+    if (ev.target) {
+      const elem = ev.target as HTMLInputElement
+      this.onFilterPaths({ query: elem.value } as AutoCompleteCompleteEvent)
+    }
+  }
+  public onClearUrl(): void {
     this.formGroup.controls['url'].setValue(this.mfeItems[0])
   }
+
   /**
    * FILTER URL (query = field value)
-   *   try to filter with best match with some exceptions:
+   *   try to filter with best match with this exception:
    *     a) empty query => list all
-   *     b) unknown entry => list all
    */
   public onFilterPaths(ev: AutoCompleteCompleteEvent): void {
     let query = ev?.query ?? undefined
@@ -428,7 +444,7 @@ export class MenuDetailComponent implements OnChanges {
       if (this.formGroup.controls['url'].value instanceof Object) query = this.formGroup.controls['url'].value.basePath
       else query = this.formGroup.controls['url'].value
     }
-    this.filteredMfes = this.filterUrl(query)
+    this.filteredMfes = this.filterUrl(query) // this split fixed a sonar complexity issue
   }
 
   private filterUrl(query: string): MFE[] {
@@ -436,15 +452,12 @@ export class MenuDetailComponent implements OnChanges {
     if (!query || query === '') {
       filtered = this.mfeItems // exception a)
     } else {
-      for (const mfeItem of this.mfeItems) {
+      for (const mfeItem of this.mfeItems)
         if (
           mfeItem.basePath?.toLowerCase().startsWith(query.toLowerCase()) ||
           query.toLowerCase().startsWith(mfeItem.basePath?.toLowerCase()!)
         )
           filtered.push(mfeItem)
-      }
-      // exception b)
-      if (filtered.length === 2 && !filtered[0].id) filtered = this.mfeItems
     }
     return filtered
   }
