513674cb96d582e7a419a75cc6b144ea2191ff5d
Henry Taeschner <henry.taeschner@capgemini.com>
2024-11-18 06:27:27 +0100

    fix: code cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const config = withModuleFederationPlugin({
     '@angular/platform-browser': { requiredVersion: 'auto', includeSecondaries: true },
     '@angular/router': { requiredVersion: 'auto', includeSecondaries: true },
     '@ngx-translate/core': { requiredVersion: 'auto' },
-    '@ngneat/error-tailor': { requiredVersion: '5.0.1', includeSecondaries: true },
+    '@ngneat/error-tailor': { requiredVersion: 'auto', includeSecondaries: true },
     primeng: { requiredVersion: 'auto', includeSecondaries: true },
     rxjs: { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
