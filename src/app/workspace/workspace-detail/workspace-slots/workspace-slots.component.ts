c885d5636d2d84dbae9d2ee43d18c15ca1173289
Henry Taeschner <github@henry-taeschner.de>
2025-07-30 13:39:28 +0200

    fix: improve workspace search and detail for a11y

--- src/app/workspace/workspace-detail/workspace-slots/workspace-slots.component.ts
+++ src/app/workspace/workspace-detail/workspace-slots/workspace-slots.component.ts
@@ -76,7 +76,7 @@ export class WorkspaceSlotsComponent implements OnInit, OnChanges, OnDestroy {
   public sortField = 'name'
   public sortOrder = -1
   public quickFilterValue: SlotFilterType = 'ALL'
-  public quickFilterItems: ExtendedSelectItem[]
+  public quickFilterOptions$: Observable<SelectItem[]> | undefined
   public quickFilterCount = ''
   public exceptionKey: string | undefined = undefined
   public sLoading = false
@@ -101,25 +101,7 @@ export class WorkspaceSlotsComponent implements OnInit, OnChanges, OnDestroy {
     this.hasEditPermission = this.user.hasPermission('WORKSPACE_SLOT#EDIT')
     this.hasCreatePermission = this.user.hasPermission('WORKSPACE_SLOT#CREATE')
     this.hasDeletePermission = this.user.hasPermission('WORKSPACE_SLOT#DELETE')
-    // quick filter
-    this.quickFilterItems = [
-      { label: 'DIALOG.SLOT.QUICK_FILTER.ALL', value: 'ALL', tooltipKey: 'DIALOG.SLOT.QUICK_FILTER.ALL.TOOLTIP' },
-      {
-        label: 'DIALOG.SLOT.QUICK_FILTER.UNREGISTERED',
-        value: 'UNREGISTERED',
-        tooltipKey: 'DIALOG.SLOT.QUICK_FILTER.UNREGISTERED.TOOLTIP'
-      },
-      {
-        label: 'DIALOG.SLOT.QUICK_FILTER.OUTDATED',
-        value: 'OUTDATED',
-        tooltipKey: 'DIALOG.SLOT.QUICK_FILTER.OUTDATED.TOOLTIP'
-      },
-      {
-        label: 'DIALOG.ROLE.QUICK_FILTER.WORKSPACE',
-        value: 'WORKSPACE',
-        tooltipKey: 'DIALOG.ROLE.QUICK_FILTER.WORKSPACE.TOOLTIP'
-      }
-    ]
+    this.prepareQuickFilter()
   }
 
   public ngOnInit() {
@@ -406,5 +388,25 @@ export class WorkspaceSlotsComponent implements OnInit, OnChanges, OnDestroy {
       'onecx-product-store-ui',
       'slots'
     )
+  }
+
+  public prepareQuickFilter(): void {
+    this.quickFilterOptions$ = this.translate
+      .get([
+        'DIALOG.SLOT.QUICK_FILTER.ALL',
+        'DIALOG.SLOT.QUICK_FILTER.WORKSPACE',
+        'DIALOG.SLOT.QUICK_FILTER.UNREGISTERED',
+        'DIALOG.SLOT.QUICK_FILTER.OUTDATED'
+      ])
+      .pipe(
+        map((data) => {
+          return [
+            { label: data['DIALOG.SLOT.QUICK_FILTER.ALL'], value: 'ALL' },
+            { label: data['DIALOG.SLOT.QUICK_FILTER.UNREGISTERED'], value: 'UNREGISTERED' },
+            { label: data['DIALOG.SLOT.QUICK_FILTER.OUTDATED'], value: 'OUTDATED' },
+            { label: data['DIALOG.SLOT.QUICK_FILTER.WORKSPACE'], value: 'WORKSPACE' }
+          ]
+        })
+      )
   }
 }
