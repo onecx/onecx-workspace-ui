8bf5ddb372169e6d8236cbc25ce288b6f7411af2
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-09-05 06:38:36 +0200

    feat: add current workspace logo RC (#678)
    * feat: add current workspace logo RC
    
    * feat: fix tests
    
    * feat: fix code smells

---
+++ src/app/remotes/current-workspace-logo/current-workspace-logo.component.main.ts
@@ -0,0 +1,1 @@
+import('./current-workspace-logo.component.bootstrap').catch((err) => console.error(err))
---
+++ src/app/remotes/current-workspace-logo/current-workspace-logo.component.ts
@@ -0,0 +1,118 @@
+import { Component, EventEmitter, inject, Inject, Input, NO_ERRORS_SCHEMA } from '@angular/core'
+import { CommonModule, Location } from '@angular/common'
+import { UntilDestroy } from '@ngneat/until-destroy'
+import { BehaviorSubject, ReplaySubject } from 'rxjs'
+
+import {
+  AngularRemoteComponentsModule,
+  BASE_URL,
+  RemoteComponentConfig,
+  ocxRemoteComponent,
+  ocxRemoteWebcomponent
+} from '@onecx/angular-remote-components'
+import { AppStateService } from '@onecx/angular-integration-interface'
+import { PortalCoreModule } from '@onecx/portal-integration-angular'
+
+import { Configuration, RefType, WorkspaceAPIService } from 'src/app/shared/generated'
+import { Utils } from 'src/app/shared/utils'
+import { environment } from 'src/environments/environment'
+
+@Component({
+  selector: 'app-current-workspace-logo',
+  templateUrl: './current-workspace-logo.component.html',
+  standalone: true,
+  imports: [AngularRemoteComponentsModule, CommonModule, PortalCoreModule],
+  providers: [
+    {
+      provide: BASE_URL,
+      useValue: new ReplaySubject<string>(1)
+    }
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+@UntilDestroy()
+export class OneCXCurrentWorkspaceLogoComponent implements ocxRemoteComponent, ocxRemoteWebcomponent {
+  // input
+  @Input() refresh: boolean | undefined = false // on any change here a reload is triggered
+  @Input() imageId: string | undefined = undefined
+  @Input() imageUrl: string | undefined = undefined
+  @Input() imageStyleClass: string | undefined = undefined
+  @Input() useDefaultLogo = false // used if logo loading failed
+  @Input() logPrefix: string | undefined = undefined
+  @Input() logEnabled = false
+  @Input() set ocxRemoteComponentConfig(config: RemoteComponentConfig) {
+    this.ocxInitRemoteComponent(config)
+  }
+  // output
+  @Input() imageLoadingFailed = new EventEmitter<boolean>()
+
+  private readonly appState = inject(AppStateService)
+
+  public workspaceName: string | undefined
+  public imageUrl$ = new BehaviorSubject<string | undefined>(undefined)
+  public defaultImageUrl: string | undefined = undefined
+
+  constructor(
+    @Inject(BASE_URL) private readonly baseUrl: ReplaySubject<string>,
+    private readonly workspaceApi: WorkspaceAPIService
+  ) {
+    this.appState.currentWorkspace$.asObservable().subscribe((workspace) => {
+      this.workspaceName = workspace?.workspaceName
+      this.imageUrl$.next(this.getImageUrl(this.workspaceName, 'url'))
+    })
+  }
+
+  ocxInitRemoteComponent(remoteComponentConfig: RemoteComponentConfig) {
+    this.baseUrl.next(remoteComponentConfig.baseUrl)
+    this.workspaceApi.configuration = new Configuration({
+      basePath: Location.joinWithSlash(remoteComponentConfig.baseUrl, environment.apiPrefix)
+    })
+    if (environment.DEFAULT_LOGO_PATH)
+      this.defaultImageUrl = Utils.prepareUrlPath(remoteComponentConfig.baseUrl, environment.DEFAULT_LOGO_PATH)
+  }
+
+  /**
+   * Image
+   */
+  public onImageLoad() {
+    this.log('onImageLoad => ok')
+    this.imageLoadingFailed.emit(false)
+  }
+
+  // try next prio level depending on previous used URL
+  public onImageLoadError(usedUrl: string): void {
+    this.log('onImageLoadError using => ' + usedUrl)
+    if (usedUrl === this.imageUrl) {
+      this.log('onImageLoadError using => image')
+      this.imageUrl$.next(this.getImageUrl(this.workspaceName, 'image'))
+    } else if (usedUrl === this.getImageUrl(this.workspaceName, 'image')) {
+      this.log('onImageLoadError using => default')
+      this.imageUrl$.next(this.getImageUrl(this.workspaceName, 'default'))
+    }
+  }
+
+  public getImageUrl(workspaceName: string | undefined, prioType: string): string | undefined {
+    this.log('getImageUrl on prioType => ' + prioType)
+
+    // if URL exist
+    if (['url'].includes(prioType) && this.imageUrl && this.imageUrl !== '') {
+      this.log('getImageUrl => ' + this.imageUrl)
+      return this.imageUrl
+    } else if (['url', 'image'].includes(prioType)) {
+      this.log(
+        'getImageUrl => ' + Utils.bffImageUrl(this.workspaceApi.configuration.basePath, workspaceName, RefType.Logo)
+      )
+      return Utils.bffImageUrl(this.workspaceApi.configuration.basePath, workspaceName, RefType.Logo)
+    } else if (['url', 'image', 'default'].includes(prioType) && this.useDefaultLogo && this.defaultImageUrl !== '') {
+      // if user wants to have the default (as asset)
+      return this.defaultImageUrl
+    }
+    this.log('getImageUrl => stop')
+    this.imageLoadingFailed.emit(true) // finally inform caller about impossibility
+    return undefined
+  }
+
+  private log(text: string) {
+    if (this.logEnabled) console.info('onecx-current-workspace-logo: ' + (this.logPrefix ?? '') + ' => ' + text)
+  }
+}
