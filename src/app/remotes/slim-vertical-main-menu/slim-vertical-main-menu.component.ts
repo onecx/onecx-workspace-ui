be783c387917d9371b86b7dc3f6c8ee4bbbc60ee
markuczy <129275100+markuczy@users.noreply.github.com>
2025-10-02 12:48:28 +0200

    feat: slim and slimplus menu modes

--- src/app/remotes/slim-vertical-main-menu/slim-vertical-main-menu.component.ts
+++ src/app/remotes/slim-vertical-main-menu/slim-vertical-main-menu.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { HttpClient } from '@angular/common/http'
-import { Component, Inject, Input } from '@angular/core'
+import { Component, inject, Inject, Input } from '@angular/core'
 import { UntilDestroy } from '@ngneat/until-destroy'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
-import { ReplaySubject } from 'rxjs'
+import { combineLatest, map, mergeMap, of, ReplaySubject } from 'rxjs'
 
 import {
   BASE_URL,
@@ -14,6 +14,13 @@ import {
 import { UserService } from '@onecx/angular-integration-interface'
 
 import { createTranslateLoader, provideTranslationPathFromMeta } from '@onecx/angular-utils'
+import { MenuService } from 'src/app/shared/services/menu.service'
+
+enum Mode {
+  SLIM = 'slim',
+  SLIM_PLUS = 'slimplus',
+  INACTIVE = 'inactive'
+}
 
 @Component({
   selector: 'app-slim-vertical-main-menu',
@@ -39,6 +46,38 @@ import { createTranslateLoader, provideTranslationPathFromMeta } from '@onecx/an
 })
 @UntilDestroy()
 export class OneCXSlimVerticalMainMenuComponent implements ocxRemoteWebcomponent {
+  Mode = Mode
+
+  private readonly menuService = inject(MenuService)
+
+  private isSlimMenuActive$ = this.menuService.isActive('slim')
+  private isSlimPlusMenuActive$ = this.menuService.isActive('slimplus')
+
+  // Assumption: only one menu can be active at a time
+  public activeMode$ = combineLatest([this.isSlimMenuActive$, this.isSlimPlusMenuActive$]).pipe(
+    map(([isSlimActive, isSlimPlusActive]) => {
+      if (isSlimActive) {
+        return Mode.SLIM
+      }
+      if (isSlimPlusActive) {
+        return Mode.SLIM_PLUS
+      }
+      return Mode.INACTIVE
+    })
+  )
+
+  // Hide the menu when inactive or when the active menu is not visible
+  public isHidden$ = this.activeMode$.pipe(
+    mergeMap((mode) => {
+      if (mode === Mode.INACTIVE) {
+        return of(true)
+      }
+
+      return this.menuService.isVisible(mode)
+    }),
+    map((isVisible) => !isVisible)
+  )
+
   constructor(
     @Inject(BASE_URL) private readonly baseUrl: ReplaySubject<string>,
     private readonly userService: UserService,
