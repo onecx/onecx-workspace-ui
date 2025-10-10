48b8c6d40b91cd13f8313ae641e5cc08cd5d18ac
markuczy <129275100+markuczy@users.noreply.github.com>
2025-10-10 19:20:05 +0200

    fix: attempt to fix code smells

--- src/app/remotes/toggle-menu-button/toggle-menu-button.component.main.ts
+++ src/app/remotes/toggle-menu-button/toggle-menu-button.component.main.ts
@@ -1,2 +1,2 @@
 /* eslint-disable-next-line prettier/prettier */
-import('./toggle-menu-button.component.bootstrap').catch((err) => console.error(err))
+import('./toggle-menu-button.component.bootstrap').catch((err) => console.error(err)) // NOSONAR
