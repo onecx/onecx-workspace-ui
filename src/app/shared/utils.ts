d9915869d3c6f787924056ff0473f7b1d69ffa4e
Henry Taeschner <github@henry-taeschner.de>
2025-10-12 10:01:21 +0200

    fix: linting

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,8 +1,7 @@
 // import { MicrofrontendDTO } from '@onecx/portal-integration-angular'
 import { AbstractControl, FormArray, FormGroup } from '@angular/forms'
 import { Location } from '@angular/common'
-import { Router } from '@angular/router'
-import { catchError, filter, first, mergeMap, Observable, of, tap } from 'rxjs'
+import { catchError, first, of, tap } from 'rxjs'
 import { SelectItem } from 'primeng/api'
 
 import { PortalMessageService, WorkspaceService } from '@onecx/angular-integration-interface'
@@ -104,38 +103,6 @@ const Utils = {
   },
   bffProductImageUrl(basePath: string | undefined, name: string | undefined): string {
     return !name ? '' : basePath + '/images/product/' + name
-  },
-
-  // goto
-  goToEndpoint(
-    workspaceService: WorkspaceService,
-    msgService: PortalMessageService,
-    router: Router,
-    productName: string,
-    appId: string,
-    endpointName: string,
-    params?: Record<string, unknown>
-  ): void {
-    workspaceService
-      .doesUrlExistFor(productName, appId, endpointName)
-      .pipe(
-        tap((exists) => {
-          if (!exists) {
-            console.error(
-              'Routing not possible for product: ' + productName + '  app: ' + appId + '  endpoint: ' + endpointName
-            )
-            msgService.error({
-              summaryKey: 'EXCEPTIONS.ENDPOINT.NOT_EXIST',
-              detailKey: 'EXCEPTIONS.CONTACT_ADMIN'
-            })
-          }
-        }),
-        filter((exists) => exists), // stop on not exists
-        mergeMap(() => workspaceService.getUrl(productName, appId, endpointName, params))
-      )
-      .subscribe((url) => {
-        router.navigateByUrl(url)
-      })
   },
 
   /**
