25a12ca075e4f8c02faa7640b821998b478dd51e
markuczy <129275100+markuczy@users.noreply.github.com>
2025-10-27 09:36:26 +0100

    fix: disable sonar for RCs main

--- src/app/remotes/slim-vertical-main-menu/slim-vertical-main-menu.component.main.ts
+++ src/app/remotes/slim-vertical-main-menu/slim-vertical-main-menu.component.main.ts
@@ -1,2 +1,2 @@
 /* eslint-disable-next-line prettier/prettier */
-import('./slim-vertical-main-menu.component.bootstrap').catch((err) => console.error(err))
+import('./slim-vertical-main-menu.component.bootstrap').catch((err) => console.error(err)) //NOSONAR
